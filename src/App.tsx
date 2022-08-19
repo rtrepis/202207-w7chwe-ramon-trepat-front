fa11bde2b4d39a5d86e9e3706640d7867f9bf85a
Ramon Trepat <r.trepis@gmail.com>
2022-08-19 21:30:52 +0200

    Added HTML component login

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
+import Login from "./components/Login/Login";
 
 function App() {
   return (
     <div className="App">
       <h1>Social Network</h1>
+      <Login />
     </div>
   );
 }
---
+++ src/components/Login/Login.tsx
@@ -0,0 +1,40 @@
+const Login = () => {
+  return (
+    <>
+      <h2>Login</h2>
+      <form>
+        <div>
+          <label htmlFor="userName" className="">
+            {" "}
+            User Name:
+            <input
+              type="text"
+              name="userName"
+              placeholder="User name"
+              required
+              autoComplete="off"
+              value="User Name"
+            />
+          </label>
+        </div>
+        <div>
+          <label htmlFor="password" className="">
+            {" "}
+            Password:
+            <input
+              type="password"
+              name="passwd"
+              placeholder="Password"
+              required
+              autoComplete="off"
+              value="Password User"
+            />
+          </label>
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </>
+  );
+};
+
+export default Login;
